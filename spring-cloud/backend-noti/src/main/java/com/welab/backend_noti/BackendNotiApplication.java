60f036584309675a7a3583c199360b71c7212a1f
danajlim <danalim0819@gmail.com>
2025-06-02 17:08:53 +0900

    initial commit

---
+++ spring-cloud/backend-noti/src/main/java/com/welab/backend_noti/BackendNotiApplication.java
@@ -0,0 +1,13 @@
+package com.welab.backend_noti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNotiApplication.class, args);
+	}
+
+}
---
+++ spring-cloud/backend-noti/src/test/java/com/welab/backend_noti/BackendNotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welab.backend_noti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
